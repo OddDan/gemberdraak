4ea6454230bafcdaf323245b19457d9e5ff8be78
DanDrory <dan@oddshapes.nl>
2016-01-30 20:29:08 +0100

    projectile particle upgrade. Model and coloring needs some work.

---
+++ gemberdraakGame/Assets/Scripts/Projectiles/RotateParent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateParent : MonoBehaviour {
+
+	Vector3 axis;
+
+	void Awake(){
+		axis = GetRandomAxis ();
+		transform.Rotate (axis);
+	}
+
+
+	Vector3 GetRandomAxis(){
+		Debug.Log (new Vector3 (Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360)));
+		return new Vector3 (Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360));
+	}
+}
