cb58bb4437e6a5fc4bbdfb764b26fbef21c29a8b
DanDrory <dan@oddshapes.nl>
2016-01-30 04:09:57 +0100

    You can now throw away sheep.

--- gemberdraakGame/Assets/Scripts/Actions/ActionController.cs
+++ gemberdraakGame/Assets/Scripts/Actions/ActionController.cs
@@ -8,21 +8,61 @@ public class ActionController : MonoBehaviour {
 	float lastFireTime = 0;
 	MovementController mc;
 
+	public float grabDistance=1;
+	public float grabSize=1;
+
 	void Awake(){
 		mc = gameObject.GetComponent<MovementController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown(mc.ctrlName + "jump") && mc.type == charType.PRIEST) {
-			if (lastFireTime < Time.realtimeSinceStartup - cooldownTime) {
-				lastFireTime = Time.realtimeSinceStartup;
-				Vector3 rotation = mc.velocity;
-				rotation.y = 0;
-				GameObject projectileShot = GameObject.Instantiate (projectile, transform.position, Quaternion.LookRotation (rotation.normalized, Vector3.up)) as GameObject;
-				Projectile pro = projectileShot.GetComponent<Projectile> ();
-				pro.mc = mc;
+		if (Input.GetButtonDown (mc.ctrlName + "jump") && mc.type == charType.PRIEST) {
+			if (!mc.carrying && mc.canMove) {
+				if (lastFireTime < Time.realtimeSinceStartup - cooldownTime) {
+					lastFireTime = Time.realtimeSinceStartup;
+					Vector3 rotation = mc.velocity;
+					rotation.y = 0;
+					GameObject projectileShot = GameObject.Instantiate (projectile, transform.position, Quaternion.LookRotation (rotation.normalized, Vector3.up)) as GameObject;
+					Projectile pro = projectileShot.GetComponent<Projectile> ();
+					pro.mc = mc;
+				}
 			}
 		}
+		if (Input.GetButtonDown (mc.ctrlName + "action") && mc.type == charType.PRIEST) {
+			if (!mc.carrying) {
+				RaycastHit[] hits;
+				hits = Physics.SphereCastAll (transform.position, grabSize, mc.body.transform.forward, grabDistance);
+				foreach (RaycastHit hit in hits) {
+					if (hit.collider.gameObject.tag == "Player") {
+						if (hit.collider.gameObject.GetComponent<MovementController> ().type == charType.SHEEP) {
+							mc.connectedPlayer = hit.collider.gameObject;
+							mc.connectedPlayer.GetComponent<MovementController> ().connectedPlayer = gameObject;
+							mc.connectedPlayer.GetComponent<MovementController> ().carrying = true;
+							mc.carrying = true;
+						}
+					}
+				}
+			} else {
+				StartCoroutine(mc.connectedPlayer.GetComponent<MovementController>().Flying(mc.throwSpeed, 22));
+				mc.connectedPlayer = null;
+				mc.carrying = false;
+			}
+		}
+
+		if (Input.GetButtonDown (mc.ctrlName + "action") && mc.type == charType.SHEEP && mc.carrying) {
+			mc.struggle += 10;
+			if (mc.struggle >= mc.maxStruggle) {
+				SetFree ();
+			}
+		}
+
 	}
+
+	void SetFree(){
+		StartCoroutine (mc.Flying(10,5));
+		mc.connectedPlayer.GetComponent<MovementController> ().SetStun ();
+		mc.struggle = 0;
+	}
+
 }
--- gemberdraakGame/Assets/Scripts/Movement/MovementController.cs
+++ gemberdraakGame/Assets/Scripts/Movement/MovementController.cs
@@ -9,6 +9,7 @@ public class MovementController : MonoBehaviour {
 	public float acceleration = 8;
 	public string ctrlName = "CTRL1_";
 	public float stuntime = 2f;
+	public float throwSpeed = 30;
 
 	public charType type = charType.PRIEST;
 
@@ -16,51 +17,28 @@ public class MovementController : MonoBehaviour {
 	public GameObject priestModel;
 	public GameObject sheepModel;
 
+	public float maxStruggle;
+	public float struggle;
+
+	public GameObject connectedPlayer;
+	public bool carrying = false;
+	public bool flying = false;
+
 	public Vector3 velocity;
 	float verticalSpeed;
 	float zSpeed;
 	float xSpeed;
-	bool canMove = true;
+	public bool canMove = true;
 
 	void Start () {
 		velocity = Vector3.zero;
 	}
 
 	void Update () {
-		if (type == charType.PRIEST) {
-			priestModel.SetActive (true);
-			sheepModel.SetActive (false);
-			body = priestModel;
-		}
-		if (type == charType.SHEEP) {
-			priestModel.SetActive (false);
-			sheepModel.SetActive (true);
-			body = sheepModel;
-		}
-		if (canMove) {
-			zSpeed = AccelerateTowards (zSpeed, acceleration, Input.GetAxis (ctrlName + "vertical") * movementSpeed);
-			xSpeed = AccelerateTowards (xSpeed, acceleration, Input.GetAxis (ctrlName + "horizontal") * movementSpeed);
-
-			velocity = new Vector3 (xSpeed, 0, zSpeed);
-			Vector3 dir = new Vector3 (xSpeed, 0, zSpeed);
 		
+		CheckType ();
+		DoMovement ();
 
-			CharacterController controller = gameObject.GetComponent<CharacterController> ();
-			if (controller.isGrounded) {
-				verticalSpeed = 0;
-				if(Input.GetButtonDown(ctrlName+"jump") && type == charType.SHEEP){
-					verticalSpeed = jumpSpeed;
-				}
-			} 
-			verticalSpeed -= gravity * Time.deltaTime;
-			velocity.y = verticalSpeed;
-
-			if (velocity.magnitude > 1) {
-				SetLookRotation (dir);
-			}
-
-			controller.Move (velocity * Time.deltaTime);
-		}
 	}
 
 	public void SetStun(){
@@ -69,6 +47,12 @@ public class MovementController : MonoBehaviour {
 
 	IEnumerator Stun(){
 		canMove = false;
+		if(carrying){
+			carrying = false;
+			connectedPlayer.gameObject.GetComponent<MovementController> ().carrying = false;
+			connectedPlayer.gameObject.GetComponent<MovementController> ().connectedPlayer = null;
+			connectedPlayer = null;
+		}
 		yield return new WaitForSeconds (stuntime);
 		canMove = true;
 	}
@@ -86,8 +70,93 @@ public class MovementController : MonoBehaviour {
 	void SetLookRotation(Vector3 target){
 		target.y = 0;
 		if (target.normalized != Vector3.zero) {
-			body.transform.rotation = Quaternion.LookRotation (target.normalized, Vector3.up);
+			if (type == charType.PRIEST || type == charType.SHEEP) {
+				body.transform.rotation = Quaternion.LookRotation (target.normalized, Vector3.up);
+			}if (type == charType.SHEEP && carrying) {
+				body.transform.rotation = Quaternion.LookRotation (target.normalized, Vector3.down);
+				body.transform.rotation *= Quaternion.Euler (0, -90, 0);
+			}
 		}
+	}
+
+	void CheckType(){
+		if (type == charType.PRIEST) {
+			priestModel.SetActive (true);
+			sheepModel.SetActive (false);
+			body = priestModel;
+		}
+		if (type == charType.SHEEP) {
+			CalculateStruggle ();
+			priestModel.SetActive (false);
+			sheepModel.SetActive (true);
+			body = sheepModel;
+		}
+	}
+
+	void DoMovement(){
+		if (canMove) {
+			if (carrying && type == charType.SHEEP) {
+
+			} else {
+				zSpeed = AccelerateTowards (zSpeed, acceleration, Input.GetAxis (ctrlName + "vertical") * movementSpeed);
+				xSpeed = AccelerateTowards (xSpeed, acceleration, Input.GetAxis (ctrlName + "horizontal") * movementSpeed);
+
+				velocity = new Vector3 (xSpeed, 0, zSpeed);
+				Vector3 dir = new Vector3 (xSpeed, 0, zSpeed);
+
+
+				CharacterController controller = gameObject.GetComponent<CharacterController> ();
+				if (controller.isGrounded) {
+					verticalSpeed = 0;
+					if (Input.GetButtonDown (ctrlName + "jump") && type == charType.SHEEP) {
+						verticalSpeed = jumpSpeed;
+					}
+				} 
+				verticalSpeed -= gravity * Time.deltaTime;
+				velocity.y = verticalSpeed;
+
+				if (velocity.magnitude > 1) {
+					SetLookRotation (dir);
+				}
+
+				controller.Move (velocity * Time.deltaTime);
+			}
+		}
+
+		if (carrying && type == charType.SHEEP){
+			SetLookRotation (connectedPlayer.GetComponent<MovementController>().body.transform.forward);
+			Vector3 targetPos = connectedPlayer.transform.position;
+			targetPos.y += 3.5f;
+			transform.position = targetPos;
+		}
+	}
+
+	void CalculateStruggle(){
+		if (carrying) {
+			if (struggle < 0) {
+				struggle = 0;
+			} else {
+				struggle -= 20 * Time.deltaTime;
+			}
+		}
+	}
+
+	public IEnumerator Flying(float forward, float height){
+		canMove = false;
+		carrying = false;
+		Vector3 dir = connectedPlayer.GetComponent<MovementController> ().velocity;
+
+		CharacterController controller = gameObject.GetComponent<CharacterController> ();
+		controller.Move (Vector3.up);
+		verticalSpeed = (height);
+		while (!controller.isGrounded) {
+			velocity = dir.normalized * forward;
+			verticalSpeed -= gravity * Time.deltaTime;
+			velocity.y = verticalSpeed;
+			controller.Move (velocity * Time.deltaTime);
+			yield return null;
+		}
+		canMove = true;
 	}
 }
 
