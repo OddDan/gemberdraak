a0b9093c0e2644dcce1dfc4177618472e4835913
Midas <midasbuitink@gmail.com>
2016-01-31 15:23:03 +0100

    death death detah

---
+++ gemberdraakGame/Assets/Scripts/Actions/Death.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<MovementController> ().type == charType.PRIEST){
+			other.gameObject.GetComponent<MovementController> ().Kill();			
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+
+	}
+
+	void Update(){
+		transform.position = transform.parent.position;
+	}
+}
