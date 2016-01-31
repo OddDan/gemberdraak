95df8f8f7ad4bea4afd0391e24eb2665ca6b5a06
Midas <midasbuitink@gmail.com>
2016-01-31 06:10:18 +0100

    added souls

--- gemberdraakGame/Assets/Scripts/Actions/Sacrifice.cs
+++ gemberdraakGame/Assets/Scripts/Actions/Sacrifice.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Sacrifice : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<MovementController> ().type == charType.SHEEP){
-			other.gameObject.GetComponent<MovementController> ().score += 1;
+			GameManager._GM.ReleaseSoul(other.gameObject.GetComponent<MovementController> ().playerID);
+			GameManager._GM.playerScores[other.gameObject.GetComponent<MovementController>().playerID-1] += 1;
 			other.gameObject.GetComponent<MovementController> ().Respawn ();
 		}
 	}
