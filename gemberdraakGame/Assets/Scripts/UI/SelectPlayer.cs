2ebb0f525626d21a19bae80369fe3cbe58764d54
DanDrory <dan@oddshapes.nl>
2016-02-01 16:18:32 +0100

    removed start screen ready disable.

--- gemberdraakGame/Assets/Scripts/UI/SelectPlayer.cs
+++ gemberdraakGame/Assets/Scripts/UI/SelectPlayer.cs
@@ -10,7 +10,7 @@ public class SelectPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () { 
 		if (Input.GetButtonDown ("CTRL" + ID +"_jump")) {
-			ready = !ready;
+			ready = true;
 		}
 		if (ready) {
 			child.SetActive (true);
